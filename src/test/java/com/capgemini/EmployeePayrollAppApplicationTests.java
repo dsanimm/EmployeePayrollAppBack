53bc219b73a8c747921aed54f458328fe7fd50f9
dsanimm <dsanimm@gmail.com>
2021-01-15 12:23:44 +0530

    added junit test

--- src/test/java/com/capgemini/EmployeePayrollAppApplicationTests.java
+++ src/test/java/com/capgemini/EmployeePayrollAppApplicationTests.java
@@ -1,13 +1,26 @@
 package com.capgemini;
 
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.capgemini.model.EmployeePayrollData;
+import com.capgemini.service.IEmployeePayrollService;
+import static org.junit.Assert.assertEquals;
+
 
 @SpringBootTest
 class EmployeePayrollAppApplicationTests {
-
+	@Autowired
+	private IEmployeePayrollService employeePayrollService;
+	
 	@Test
-	void contextLoads() {
+	public void No_of_employees_match() {
+		List<EmployeePayrollData> list =employeePayrollService.getEmployeeData();
+		Assert.assertEquals(3, list.size());
 	}
 
 }
