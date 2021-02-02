385fdc93d45abc2e3a52b31c618dd0d11e16a292
dsanimm <dsanimm@gmail.com>
2021-02-02 17:34:25 +0530

    Update EmployeePayrollController.java

--- src/main/java/com/capgemini/controller/EmployeePayrollController.java
+++ src/main/java/com/capgemini/controller/EmployeePayrollController.java
@@ -20,7 +20,7 @@ import com.capgemini.model.EmployeePayrollData;
 import com.capgemini.service.EmailService;
 import com.capgemini.service.IEmployeePayrollService;
 
-@CrossOrigin(origins = "http://172.18.0.3:8080")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
